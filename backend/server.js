558aa32a1e4f1ff1c5110bbd71e8d268ebc55acf
Akshaya sri A <akshayanatalia356@gmail.com>
2025-04-17 17:52:35 +0530

    Before final commit

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,17 @@ app.use("/api", jobRoutes);
 
 // Serve static files in uploads folder
 app.use('/uploads', express.static(path.join(path.resolve(), '/uploads')));
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+}
+else{
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
